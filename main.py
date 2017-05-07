8323511591728a6b7ffb2d802482023ca247fb61
Misha Wagner <mishajw@gmail.com>
2017-05-07 16:24:03 +0800

    Create model class for U-Net with downsampling complete

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from unet_model import UnetModel
 import tensorflow as tf
 
 
 def main():
-    pass
+    input_image = tf.placeholder(dtype="float32", shape=[None, 256, 256, 1], name="input_image")
+    model = UnetModel(input_image)
+
+    print("Model input size: %s" % input_image.shape)
+    print("Model output size: %s" % model.output.shape)
 
 
 if __name__ == "__main__":
---
+++ unet_model.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+
+
+class UnetModel:
+    def __init__(self, input_image):
+        self.num_sections = 4
+        self.num_layers_per_section = 2
+        self.start_num_filters = 64
+
+        sections = self.__create_all_sections(input_image)
+        self.output = self.__create_flattening_layer(sections)
+
+    def __create_all_sections(self, input_image):
+        current_num_filters = self.start_num_filters
+        current_input = input_image
+
+        down_sections_output = []
+
+        for section in range(self.num_sections):
+            print("Input shape at down section %d: %s" % (section, current_input.shape))
+
+            with tf.name_scope("section_%d" % section):
+                current_input = self.__create_layer_section(current_input, current_num_filters)
+
+            down_sections_output.append(current_input)
+
+            # If it's not the last section
+            if section != self.num_sections - 1:
+                current_input = tf.nn.max_pool(
+                    current_input,
+                    ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1],
+                    padding="SAME",
+                    name="max_pool_%d_to_%d" % (section, section + 1))
+
+                current_num_filters *= 2
+
+            print("Output shape at down section %d: %s" % (section, current_input.shape))
+
+            print()
+
+        for section in range(self.num_sections - 1):
+            print("Input shape at up section %d: %s" % (section, current_input.shape))
+
+            upsample_size = tf.stack([
+                -1,
+                current_input.shape[1] * 2,
+                current_input.shape[2] * 2,
+                current_input.shape[3]])
+
+            upsample_filter = tf.Variable(
+                tf.random_normal([2, 2, 1, current_num_filters], dtype="float32"),
+                name="upsample_filter")
+
+            current_input = tf.nn.conv2d_transpose(
+                current_input,
+                filter=upsample_filter,
+                output_shape=upsample_size,
+                strides=[1, 2, 2, 1],
+                padding="SAME",
+                name="max_pool_%d_to_%d" % (section, section + 1))
+
+            current_num_filters = int(current_num_filters / 2)
+
+            corresponding_down_section = down_sections_output[-(section + 2)]
+
+            print("Adding shapes at up section %d: %s + %s" %
+                (section, corresponding_down_section.shape, current_input.shape))
+
+            current_input = tf.concat([corresponding_down_section, current_input], 3)
+
+            print("Result of add at up section %d: %s" % (section, current_input.shape))
+
+            current_input = self.__create_layer_section(current_input, current_num_filters)
+
+            print("Output shape at up section %d: %s" % (section, current_input.shape))
+
+            print()
+
+        return current_input
+
+    def __create_layer_section(self, section_input, layer_size):
+        current_input = section_input
+
+        for _ in range(self.num_layers_per_section):
+            current_input = tf.layers.conv2d(
+                current_input,
+                filters=layer_size,
+                kernel_size=[3, 3],
+                padding="SAME",
+                activation=tf.nn.relu)
+
+        return current_input
+
+    @staticmethod
+    def __create_flattening_layer(layer_input):
+        return tf.layers.conv2d(
+            layer_input,
+            filters=1,
+            kernel_size=[1, 1],
+            activation=tf.nn.relu)
