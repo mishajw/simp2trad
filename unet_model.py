44fe16e59b1eb9d4cd2368d756f6731d7bc24c8e
Misha Wagner <mishajw@gmail.com>
2017-05-08 10:16:16 +0800

    Fix bugs in U-Net model and improve naming

--- unet_model.py
+++ unet_model.py
@@ -20,61 +20,72 @@ class UnetModel:
         down_sections_output = []
 
         for section in range(self.num_sections):
-            log.debug("Input shape at down section %d: %s" % (section, current_input.shape))
+            with tf.variable_scope("down_section_%d" % section):
+                log.debug("Input shape at down section %d: %s" % (section, current_input.shape))
 
-            with tf.name_scope("section_%d" % section):
-                current_input = self.__create_layer_section(current_input, current_num_filters)
-
-            down_sections_output.append(current_input)
+                with tf.variable_scope("layers"):
+                    current_input = self.__create_layer_section(current_input, current_num_filters)
+                    down_sections_output.append(current_input)
 
             # If it's not the last section
-            if section != self.num_sections - 1:
-                current_input = tf.nn.max_pool(
-                    current_input,
-                    ksize=[1, 2, 2, 1],
-                    strides=[1, 2, 2, 1],
-                    padding="SAME",
-                    name="max_pool_%d_to_%d" % (section, section + 1))
+            with tf.name_scope("max_pool_%d_to_%d" % (section, section + 1)):
+                if section != self.num_sections - 1:
+                    current_input = tf.nn.max_pool(
+                        current_input,
+                        ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1],
+                        padding="SAME")
 
-                current_num_filters *= 2
+                    current_num_filters *= 2
 
             log.debug("Output shape at down section %d: %s" % (section, current_input.shape))
 
-        for section in range(self.num_sections - 1):
+        for section in range(1, self.num_sections):
             log.debug("Input shape at up section %d: %s" % (section, current_input.shape))
 
-            upsample_size = tf.stack([
-                -1,
-                current_input.shape[1] * 2,
-                current_input.shape[2] * 2,
-                current_input.shape[3]])
+            with tf.variable_scope("upsample_%d_to_%d" % (section - 1, section)):
+                upsample_size_tensor = tf.stack([
+                    tf.shape(current_input)[0],
+                    tf.shape(current_input)[1] * 2,
+                    tf.shape(current_input)[2] * 2,
+                    tf.shape(current_input)[3]])
 
-            upsample_filter = tf.Variable(
-                tf.random_normal([2, 2, 1, current_num_filters], dtype="float32"),
-                name="upsample_filter")
+                upsample_size_array = [
+                    current_input.shape[0],
+                    current_input.shape[1] * 2,
+                    current_input.shape[2] * 2,
+                    current_input.shape[3]]
 
-            current_input = tf.nn.conv2d_transpose(
-                current_input,
-                filter=upsample_filter,
-                output_shape=upsample_size,
-                strides=[1, 2, 2, 1],
-                padding="SAME",
-                name="max_pool_%d_to_%d" % (section, section + 1))
+                upsample_filter = tf.Variable(
+                    tf.random_normal([2, 2, current_num_filters, current_num_filters], dtype="float32"),
+                    name="upsample_filter_%d_to_%d" % (section, section + 1))
+
+                current_input = tf.nn.conv2d_transpose(
+                    current_input,
+                    filter=upsample_filter,
+                    output_shape=upsample_size_tensor,
+                    strides=[1, 2, 2, 1],
+                    padding="SAME",
+                    name="upsample_%d_to_%d" % (section, section + 1))
+
+                current_input.set_shape(upsample_size_array)
 
             current_num_filters = int(current_num_filters / 2)
 
-            corresponding_down_section = down_sections_output[-(section + 2)]
+            with tf.variable_scope("up_section_%d" % section):
+                corresponding_down_section = down_sections_output[-(section + 1)]
 
-            log.debug("Adding shapes at up section %d: %s + %s" %
-                  (section, corresponding_down_section.shape, current_input.shape))
+                log.debug("Adding shapes at up section %d: %s + %s" %
+                      (section, corresponding_down_section.shape, current_input.shape))
 
-            current_input = tf.concat([corresponding_down_section, current_input], 3)
+                current_input = tf.concat([corresponding_down_section, current_input], 3, name="up_down_link")
 
-            log.debug("Result of add at up section %d: %s" % (section, current_input.shape))
+                log.debug("Result of add at up section %d: %s" % (section, current_input.shape))
 
-            current_input = self.__create_layer_section(current_input, current_num_filters)
+                with tf.variable_scope("layers"):
+                    current_input = self.__create_layer_section(current_input, current_num_filters)
 
-            log.debug("Output shape at up section %d: %s" % (section, current_input.shape))
+                log.debug("Output shape at up section %d: %s" % (section, current_input.shape))
 
         return current_input
 
@@ -97,4 +108,5 @@ class UnetModel:
             layer_input,
             filters=1,
             kernel_size=[1, 1],
-            activation=tf.nn.relu)
+            activation=tf.nn.relu,
+            name="flattening_layer")
